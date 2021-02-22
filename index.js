92cfcf7047e5f69657eab23f0c19fc0c65eae02b
“zed” <zed@kolk.me>
2021-02-22 00:00:49 -0600

    second

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function saturdayFun(activity= "roller-skate") {
     console.log(`This Saturday, I want to ${activity}!`);
 }
+
+function mondayWork(activity="go to the office") {
+    console.log(`"This Monday, I will ${activity}."`);
+}
